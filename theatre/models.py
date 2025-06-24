999229cff0f75e4736609e8cbf03b7eba0a0a609
Taras Shevchyk <tarashevchik27@gmail.com>
2025-06-25 02:24:14 +0300

    implement __str__ method for all models

--- theatre/models.py
+++ theatre/models.py
@@ -6,9 +6,15 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return str(self.name)
 
 
 class Play(models.Model):
@@ -17,22 +23,35 @@ class Play(models.Model):
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
 
+    def __str__(self) -> str:
+        return str(self.title)
+
 
 class TheatreHall(models.Model):
     name = models.CharField(max_length=255)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
+    def __str__(self) -> str:
+        return (
+            f"Hall: {self.name} rows:{self.rows} with {self.seats_in_row} seats in row"
+        )
+
 
 class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.created_at)
 
 class Performance(models.Model):
     play = models.ForeignKey(Play, on_delete=models.CASCADE)
     theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE)
     showtime = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f"Perfomance {self.play} on {self.showtime}"
 
 
 class Ticket(models.Model):
@@ -40,3 +59,6 @@ class Ticket(models.Model):
     seat = models.IntegerField()
     performance = models.ForeignKey(Performance, on_delete=models.DO_NOTHING)
     reservation = models.ForeignKey(Reservation, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"{self.performance}, row({self.row}), seat({self.seat})"     
