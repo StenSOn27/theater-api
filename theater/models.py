f32aa3ba462da628251fee40e7e6fc43939213a2
StenSOn27 <tarashevchik27@gmail.com>
2025-08-10 01:47:26 +0300

    update Ticket model: add validate_tickets func redefine clean, save and str methods, add ordering

--- theater/models.py
+++ theater/models.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.db import models
 import os
 from django.utils.text import slugify
+from django.core.exceptions import ValidationError
+
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
@@ -103,8 +105,59 @@ class Performance(models.Model):
 class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
-    performance = models.ForeignKey(Performance, on_delete=models.DO_NOTHING)
-    reservation = models.ForeignKey(Reservation, on_delete=models.DO_NOTHING)
+    performance = models.ForeignKey(
+        Performance,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    @staticmethod
+    def validate_ticket(row, seat, theater_hall, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, theater_hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(theater_hall, theater_hall_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {theater_hall_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.performance.theater_hall,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        *args,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
 
     def __str__(self):
-        return f"Row({self.row}), seat({self.seat})"
+        return (
+            f"{str(self.performance)} (row: {self.row}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("performance", "row", "seat")
+        ordering = ["row", "seat"]
